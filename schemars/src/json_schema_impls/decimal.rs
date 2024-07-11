9ba22b3fd86c38d0eb38089cfe1e536fe811bda0
Fabian <spiele2014fs@gmail.com>
2024-07-11 19:51:15 +0200

    Add rust_decimal_float feature

--- schemars/src/json_schema_impls/decimal.rs
+++ schemars/src/json_schema_impls/decimal.rs
@@ -31,8 +31,29 @@ macro_rules! decimal_impl {
     };
 }
 
-#[cfg(feature = "rust_decimal")]
+#[cfg(all(feature = "rust_decimal", not(feature = "rust_decimal_float")))]
 decimal_impl!(rust_decimal::Decimal);
+#[cfg(all(feature = "rust_decimal", feature = "rust_decimal_float"))]
+impl JsonSchema for rust_decimal::Decimal {
+    no_ref_schema!();
+
+    fn schema_name() -> String {
+        "Decimal".to_owned()
+    }
+
+    fn schema_id() -> Cow<'static, str> {
+        Cow::Borrowed("Decimal")
+    }
+
+    fn json_schema(_: &mut SchemaGenerator) -> Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::Number.into()),
+            ..Default::default()
+        }
+        .into()
+    }
+}
+
 #[cfg(feature = "bigdecimal03")]
 decimal_impl!(bigdecimal03::BigDecimal);
 #[cfg(feature = "bigdecimal04")]
